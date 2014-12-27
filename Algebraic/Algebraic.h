ef23e7cffa2921f45b9af73448adfbe97cc2f8c0
Jason Larsen <jason.larsen@gmail.com>
2014-12-26 19:22:03 -0800

    Initial Commit

---
+++ Algebraic/Algebraic.h
@@ -0,0 +1,19 @@
+//
+//  Algebraic.h
+//  Algebraic
+//
+//  Created by Jason Larsen on 12/26/14.
+//  Copyright (c) 2014 Heartbit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Algebraic.
+FOUNDATION_EXPORT double AlgebraicVersionNumber;
+
+//! Project version string for Algebraic.
+FOUNDATION_EXPORT const unsigned char AlgebraicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Algebraic/PublicHeader.h>
+
+
